74771c08325222aa8e1b539fbd3a6523b589acff
Program Easily <programeasilyofficial@gmail.com>
2021-07-12 20:49:17 +0530

    Changing image location

--- src/components/teleport/TeleportChild.vue
+++ src/components/teleport/TeleportChild.vue
@@ -1,25 +1,16 @@
 <template>
   <img
-    :src="imagePathComputed"
+    :src="imagePath"
     alt="Teleport child Text"
     :style="{ width: '100%', height: '100%' }"
   />
 </template>
 
 <script>
-import { computed } from "vue";
 export default {
   props: {
     imagePath: String,
     altText: String,
-  },
-  setup(props) {
-    let imagePathComputed = computed(() => {
-      return require(`@/assets/${props.imagePath}`);
-    });
-    return {
-      imagePathComputed,
-    };
   },
 };
 </script>
